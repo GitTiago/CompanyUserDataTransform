ab3823ded05cf852281bf5b58156fe59eb01c028
Tiago <tiagomanuelfs@hotmail.com>
2021-06-06 20:30:21 +0100

    Add test on adding company to the user

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,13 +1,13 @@
 import json
-import tempfile
+from tempfile import TemporaryFile
 from datetime import date
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 
-from transform import add_fullname, remove_under_30s
+from transform import add_fullname, remove_under_30s, transform_user_to_include_company
 
 
 def test_add_fullname():
-    with tempfile.TemporaryFile(mode='w+') as user_json_file, tempfile.TemporaryFile(mode='w+') as output_file:
+    with TemporaryFile(mode='w+') as user_json_file, TemporaryFile(mode='w+') as output_file:
         users_json = """
         [
         {
@@ -27,20 +27,20 @@ def test_add_fullname():
         ]
         """
         base_user_json_dict = json.loads(users_json)
-        expected_output_dict = [{**user_dict, **{"full_name": f"{user_dict['forename']} {user_dict['surname']}"}}
-                                for user_dict in base_user_json_dict]
+        expected_output = [{**user_dict, **{"full_name": f"{user_dict['forename']} {user_dict['surname']}"}}
+                           for user_dict in base_user_json_dict]
 
         user_json_file.write(users_json)
         user_json_file.seek(0)
         add_fullname(user_json_file, output_file)
         output_file.seek(0)
 
-        added_fullname_dict = json.load(output_file)
-        assert expected_output_dict == added_fullname_dict
+        added_fullname_list = json.load(output_file)
+        assert expected_output == added_fullname_list
 
 
 def test_remove_under_30s():
-    with tempfile.TemporaryFile(mode='w+') as user_json_file, tempfile.TemporaryFile(mode='w+') as output_file:
+    with TemporaryFile(mode='w+') as user_json_file, TemporaryFile(mode='w+') as output_file:
         users_json = """
         [
         {
@@ -59,8 +59,8 @@ def test_remove_under_30s():
         }
         ]
         """
-        base_user_json_dict = json.loads(users_json)
-        expected_output_dict = [base_user_json_dict[1]]
+        base_user_json_list = json.loads(users_json)
+        expected_output = [base_user_json_list[1]]
 
         user_json_file.write(users_json)
         user_json_file.seek(0)
@@ -74,5 +74,69 @@ def test_remove_under_30s():
             remove_under_30s(user_json_file, output_file)
         output_file.seek(0)
 
-        added_fullname_dict = json.load(output_file)
-        assert expected_output_dict == added_fullname_dict
+        under_30s_dict = json.load(output_file)
+        assert expected_output == under_30s_dict
+
+
+def test_transform_user_to_include_company():
+    with TemporaryFile(mode='w+') as user_json_file, TemporaryFile(mode='w+') as output_file, \
+            TemporaryFile(mode='w+') as company_json_file:
+        users_json = """
+        [
+        {
+            "forename": "Jane",
+            "surname": "Smith",
+            "date_of_birth": "2001/10/12",
+            "location": "London",
+            "company_id": 3
+        },
+        {
+            "forename": "Mark",
+            "surname": "Johnson",
+            "date_of_birth": "1987/01/28",
+            "location": "New York",
+            "company_id": 1
+        }
+        ]
+        """
+        company_json = """
+        [
+            {
+                "id": 1,
+                "name": "Company1",
+                "headquarters": "City",
+                "industry": "The Industry"
+            },
+            {
+                "id": 2,
+                "name": "Company2",
+                "headquarters": "City",
+                "industry": "The Industry"
+            },
+            {
+                "id": 3,
+                "name": "Company3",
+                "headquarters": "City",
+                "industry": "The Industry"
+            }
+        ]
+        """
+        base_user_json_list = json.loads(users_json)
+        company_json_list = json.loads(company_json)
+        expected_output_dict = [*base_user_json_list]
+
+        del expected_output_dict[0]["company_id"]
+        expected_output_dict[0]["company"] = company_json_list[2]
+        del expected_output_dict[1]["company_id"]
+        expected_output_dict[1]["company"] = company_json_list[0]
+
+        user_json_file.write(users_json)
+        user_json_file.seek(0)
+        company_json_file.write(company_json)
+        company_json_file.seek(0)
+
+        transform_user_to_include_company(user_json_file, company_json_file, output_file)
+        output_file.seek(0)
+
+        added_company_dict = json.load(output_file)
+        assert expected_output_dict == added_company_dict
