c727609c5e27cbe18f2eeae4ab5fab544aebbdd0
Tiago <tiagomanuelfs@hotmail.com>
2021-06-06 19:09:05 +0100

    Test domain with pytest

---
+++ tests/test_domain.py
@@ -0,0 +1,63 @@
+from datetime import date, timedelta
+
+import pytest
+from pydantic import ValidationError
+
+from domain import User
+
+
+def test_user_date_format():
+    valid_user_dict = {
+        "forename": "Git",
+        "surname": "Tiago",
+        "location": "United Kingdom",
+        "company_id": 1
+    }
+
+    with pytest.raises(ValidationError) as e_info:
+        User(**valid_user_dict, date_of_birth="1970-01-01")
+    
+    assert "time data '1970-01-01' does not match format '%Y/%m/%d'" in str(e_info.value)
+
+
+def test_user_older_than():
+    # Test all reasonable ages
+    for age in range(0, 100):
+        exact_date = date.today() - timedelta(days=365 * age)
+        more_than_date = date.today() - timedelta(days=365 * age + 1)
+
+        exactly_age_user = User(
+            forename="Git",
+            surname="Tiago",
+            date_of_birth=exact_date,
+            location="United Kingdom",
+            company_id=1,
+        )
+
+        older_than_date_user = User(**{**exactly_age_user.dict(), **{"date_of_birth": more_than_date}})
+
+        assert not exactly_age_user.is_older_than(age)
+        assert older_than_date_user.is_older_than(age)
+
+
+def test_full_name_creation():
+    static_full_name_user = User(
+        forename="Git",
+        surname="Tiago",
+        date_of_birth="1970/01/01",
+        location="United Kingdom",
+        company_id=1,
+    )
+
+    assert static_full_name_user.full_name == "Git Tiago"
+
+    static_full_name_user = User(
+        forename="Git",
+        surname="Tiago",
+        full_name="Git Lab Tiago",
+        date_of_birth="1970/01/01",
+        location="United Kingdom",
+        company_id=1,
+    )
+
+    assert static_full_name_user.full_name == "Git Lab Tiago"
